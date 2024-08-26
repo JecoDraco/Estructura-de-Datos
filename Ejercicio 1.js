3b5addd9c308fa5ab9f86d9e58d5855a3645499b
Jose Eduardo <jeco2002eduardo@gmail.com>
2024-08-25 20:45:14 -0600

    Ejercicio

---
+++ Ejercicio 1.js
@@ -0,0 +1,16 @@
+let A = [[1, 2], [3, 4]];
+
+for(let i = 0; i < A.length; i++){
+    console.log(`Arreglo ${i}`)
+
+    for(let o = 0; o < A[i].length; o++){
+        console.log(A[i][o]);
+    }
+    
+}
+
+//for of
+//for in
+//for each
+//funcion flecha
+//while
