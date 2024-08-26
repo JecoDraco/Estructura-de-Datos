815000c0ba97b3c377f9e5858f42aa1f18e06601
Jose Eduardo <jeco2002eduardo@gmail.com>
2024-08-25 21:22:44 -0600

    Ejercicio

--- Ejercicio 1.js
+++ Ejercicio 1.js
@@ -1,4 +1,4 @@
-let A = [[1, 2], [3, 4]];
+const A = [[1, 2], [3, 4]];
 
 for(let i = 0; i < A.length; i++){
     console.log(`Arreglo ${i}`)
@@ -10,7 +10,45 @@ for(let i = 0; i < A.length; i++){
 }
 
 //for of
+
+for (const [index, fila] of A.entries()) {
+    console.log(`Arreglo ${index}`);
+    
+    for (const elemento of fila) {
+        console.log(elemento);
+    }
+}
+
 //for in
-//for each
-//funcion flecha
+
+for(const i in A){
+    console.log(`Arreglo ${i}`)
+    for(const o in A[i]){
+        console.log(A[i][o]) 
+    }
+}
+
+//for each //funcion flecha
+
+A.forEach((fila, i) => {
+    console.log(`Arreglo ${i}`);
+    
+    fila.forEach(elemento => {
+        console.log(elemento);
+    });
+});
+
 //while
+
+let i = 0;
+while (i < A.length) {
+    console.log(`Arreglo ${i}`);
+    
+    let o = 0;
+    while (o < A[i].length) {
+        console.log(A[i][o]);
+        o++;
+    }
+    
+    i++;
+}
